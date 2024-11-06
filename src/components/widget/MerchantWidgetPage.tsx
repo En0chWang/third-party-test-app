b1144999c7551b3b4ee8070812a308597c11e86a
Enoch Wang <promisey@amazon.com>
2024-11-05 18:35:03 -0800

    Integrated with SDK

--- src/components/widget/MerchantWidgetPage.tsx
+++ src/components/widget/MerchantWidgetPage.tsx
@@ -3,6 +3,8 @@ import { useLocation } from 'react-router-dom';
 import { Container, Row, Col, Table, Spinner } from 'react-bootstrap';
 import { fetchAuthSession, getCurrentUser } from 'aws-amplify/auth';
 import { get } from 'aws-amplify/api';
+import { EmbeddedAppSDK } from '@amzn/seller-central-embedded-app-sdk';
+import { AuthResponseMessage } from '@amzn/seller-central-embedded-app-sdk-models'
 
 interface MerchantItem {
     mcid: string;
@@ -27,31 +29,50 @@ const MerchantWidgetPage: React.FC = () => {
             const currentUser = await getCurrentUser();
             const user_id = currentUser.userId;
 
+            const embeddedAppSDK: EmbeddedAppSDK = EmbeddedAppSDK.getInstance();
+
             try {
-                const restOperation = get({ 
-                    apiName: 'myRestApi',
-                    path: 'merchants-path',
-                    options: {
-                        headers: {
-                            'Authorization': token
-                        },
-                        queryParams: {
-                            'user_id': user_id,
-                        }
+                embeddedAppSDK.initialize();
+                console.log("==== [3P] Embedded App SDK initialized successfully");
+                console.log("==== [3P] Fetching auth code now");
+              
+                const myType: Promise<AuthResponseMessage> = embeddedAppSDK.getInstance();
+                myType.then(async (authCode) => {
+                  console.log('Authenticated user token value: ', authCode);
+                  if (authCode) {
+                    console.log('auth code is not null: ', authCode);
+                    try {
+                        const restOperation = get({ 
+                            apiName: 'myRestApi',
+                            path: 'merchants-path',
+                            options: {
+                                headers: {
+                                    'Authorization': token
+                                },
+                                queryParams: {
+                                    'user_id': user_id,
+                                }
+                            }
+                        });
+                        const { body } = await restOperation.response;
+                        const str = await body.text();
+                        // console.log(JSON.parse(str));
+                        setLoading(false);
+                        setApiData(JSON.parse(str));
+                    } catch (error) {
+                        console.log(error);
+                        setLoading(false);
+                        setApiData({message: [], error: 'There is something wrong with Widget'});
                     }
+                  }
+                  
+                }).catch((error: Error) => {
+                  console.error('==== [3P] Error retrieving auth token:', error);
                 });
-                const { body } = await restOperation.response;
-                const str = await body.text();
-                console.log(JSON.parse(str));
-                setLoading(false);
-                setApiData(JSON.parse(str));
             } catch (error) {
-                console.log(error);
-                setLoading(false);
-                setApiData({message: [], error: 'There is something wrong with Widget'});
+                console.error("==== [3P] Error during Embedded App SDK initialization:", error);
             }
         };
-
         getMerchants();
     }, [location]);
 
