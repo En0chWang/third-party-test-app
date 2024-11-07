59de5be14ddfa385fbff5ca6041c2bb813231b48
Enoch Wang <promisey@amazon.com>
2024-11-07 01:34:19 -0800

    Revert "updated build commands"
    This reverts commit 191115750f61b2a4a17f13e85f9454c5aaf5d3ad.
    
    Revert "updated amplify.yml"
    
    This reverts commit ee4b5e42507e5e49fa0ed825edd4888536628925.
    
    Revert "minor fix"
    
    This reverts commit 4b99cc25d1acf693da899422fdb10a9e47b3843a.
    
    Revert "updated amplify.yml"
    
    This reverts commit 4ce7540a089ef209a21f67369e27cc2ad23f5e9f.
    
    Revert "minor fix"
    
    This reverts commit 5ca31c869226028b6d7baf381644e7b9a8710dfc.
    
    Revert "minor fix"
    
    This reverts commit aa8fbb1128d23cbc8f10238f4acc4ad14ef164a6.
    
    Revert "updated sdk"
    
    This reverts commit ad6e3a65eeb5f3baaafe8257066a6cd17ea53285.
    
    Revert "Integrated with SDK"
    
    This reverts commit b1144999c7551b3b4ee8070812a308597c11e86a.

--- src/components/widget/MerchantWidgetPage.tsx
+++ src/components/widget/MerchantWidgetPage.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Container, Row, Col, Table, Spinner } from 'react-bootstrap';
-// import { fetchAuthSession, getCurrentUser } from 'aws-amplify/auth';
-// import { get } from 'aws-amplify/api';
-import { EmbeddedAppSDK } from '@amzn/seller-central-embedded-app-sdk';
+import { fetchAuthSession, getCurrentUser } from 'aws-amplify/auth';
+import { get } from 'aws-amplify/api';
 
 interface MerchantItem {
     mcid: string;
@@ -22,41 +21,37 @@ const MerchantWidgetPage: React.FC = () => {
 
     useEffect(() => {
         const getMerchants = async ()=> {
-
-            // const session = await fetchAuthSession();
-            // const token = session.tokens?.idToken?.toString() ?? '';
+            const session = await fetchAuthSession();
+            const token = session.tokens?.idToken?.toString() ?? '';
     
-            // const currentUser = await getCurrentUser();
-            // const user_id = currentUser.userId;
-
-            const embeddedAppSDK: EmbeddedAppSDK = EmbeddedAppSDK.getInstance();
+            const currentUser = await getCurrentUser();
+            const user_id = currentUser.userId;
 
             try {
-                // Initialize the Embedded App SDK
-                embeddedAppSDK.initialize().then(() => {
-                    console.log("[3P] Embedded App SDK initialized successfully");
-
-                    embeddedAppSDK.authorizationModule.getAuthCode().then(authCode => {
-                        console.log('[3P]Authenticated user token value:', authCode);
-
-                        setApiData({
-                            'message': [
-                                {'mcid': authCode?.value ?? '', 'updated_time': 123}
-                            ],
-                            'error': ''
-                        });
-                        setLoading(false);
-                      }).catch(error => {
-                        console.error('[3P]Error retrieving auth token:', error);
-                    });
-
-                 }).catch(error => {
-                    console.error("[3P] Error during Embedded App SDK initialization:", error);
+                const restOperation = get({ 
+                    apiName: 'myRestApi',
+                    path: 'merchants-path',
+                    options: {
+                        headers: {
+                            'Authorization': token
+                        },
+                        queryParams: {
+                            'user_id': user_id,
+                        }
+                    }
                 });
+                const { body } = await restOperation.response;
+                const str = await body.text();
+                console.log(JSON.parse(str));
+                setLoading(false);
+                setApiData(JSON.parse(str));
             } catch (error) {
-                console.error("==== [3P] Error during Embedded App SDK initialization:", error);
+                console.log(error);
+                setLoading(false);
+                setApiData({message: [], error: 'There is something wrong with Widget'});
             }
         };
+
         getMerchants();
     }, [location]);
 
